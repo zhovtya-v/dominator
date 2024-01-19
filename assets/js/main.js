dd247f84825056367e2ae279bc0cab0914b38cd8
victoria.v <victoria.zhovtya.devmrks@gmail.com>
2024-01-19 09:45:00 +0200

    init commit

---
+++ assets/js/main.js
@@ -0,0 +1,18 @@
+let menuBtn = document.querySelector('.nav-btn-js');
+let menu = document.querySelector('.menu');
+let menuItem = document.querySelectorAll('.menu a');
+
+menuBtn.addEventListener('click', function () {
+    console.log('menuBtn', menuBtn)
+
+    menuBtn.classList.toggle('active');
+    menu.classList.toggle('active');
+})
+
+
+menuItem.forEach(function (menuItem) {
+    menuItem.addEventListener('click', function () {
+        menuBtn.classList.toggle('active');
+        menu.classList.toggle('active');
+    })
+})
