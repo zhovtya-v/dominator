bf2428697bcd67c07d5bed62441ae047dfad1f5e
victoria.v <victoria.zhovtya.devmrks@gmail.com>
2024-02-22 17:32:47 +0200

    Merge branch 'master' of github.com:zhovtya-v/dominator into animation
    # Conflicts:
    #	assets/scss/component/home-page/_hero.scss
    #	style.css.map
    #	style.scss

--- assets/js/main.js
+++ assets/js/main.js
@@ -262,3 +262,98 @@ function road_map_reinit_slider() {
         })
     }
 }
+
+/*
+function ShimmerElement(q){
+    this.anime=false;
+    this.el=document.querySelector(q);
+    if(this.el==null)return false;
+    this.start=function(){
+        if(this.anime)this.anime.cancel();
+        //створюємо анімацію
+        this.anime=this.el.animate([{opacity:0.2},{opacity:1},{opacity:0},{opacity:0.5},{opacity:0},{opacity:0},{opacity:0},{opacity:1}],{duration:1600, iterations:'Infinity'});
+    };
+    this.stop=function(){
+        this.anime.cancel();
+    }
+    this.startTime=function(ms){
+        this.start();
+        setTimeout(this.stop.bind(this), ms);
+    }
+
+}
+
+var f3=new ShimmerElement('.title');
+f3.startTime(4000);*/
+
+
+
+
+function init(){
+    new SmoothScroll(document,100,20)
+}
+
+function SmoothScroll(target, speed, smooth) {
+    if (target === document)
+        target = (document.scrollingElement
+            || document.documentElement
+            || document.body.parentNode
+            || document.body) // cross browser support for document scrolling
+
+    var moving = false
+    var pos = target.scrollTop
+    var frame = target === document.body
+    && document.documentElement
+        ? document.documentElement
+        : target // safari is the new IE
+
+    target.addEventListener('mousewheel', scrolled, { passive: false })
+    target.addEventListener('DOMMouseScroll', scrolled, { passive: false })
+
+    function scrolled(e) {
+        e.preventDefault(); // disable default scrolling
+
+        var delta = normalizeWheelDelta(e)
+
+        pos += -delta * speed
+        pos = Math.max(0, Math.min(pos, target.scrollHeight - frame.clientHeight)) // limit scrolling
+
+        if (!moving) update()
+    }
+
+    function normalizeWheelDelta(e){
+        if(e.detail){
+            if(e.wheelDelta)
+                return e.wheelDelta/e.detail/40 * (e.detail>0 ? 1 : -1) // Opera
+            else
+                return -e.detail/3 // Firefox
+        }else
+            return e.wheelDelta/120 // IE,Safari,Chrome
+    }
+
+    function update() {
+        moving = true
+
+        var delta = (pos - target.scrollTop) / smooth
+
+        target.scrollTop += delta
+
+        if (Math.abs(delta) > 0.5)
+            requestFrame(update)
+        else
+            moving = false
+    }
+
+    var requestFrame = function() { // requestAnimationFrame cross browser
+        return (
+            window.requestAnimationFrame ||
+            window.webkitRequestAnimationFrame ||
+            window.mozRequestAnimationFrame ||
+            window.oRequestAnimationFrame ||
+            window.msRequestAnimationFrame ||
+            function(func) {
+                window.setTimeout(func, 1000 / 50);
+            }
+        );
+    }()
+}
