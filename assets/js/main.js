03e970de85c245f55019708f44bb1078c2363b55
victoria.v <victoria.zhovtya.devmrks@gmail.com>
2024-02-29 16:58:58 +0200

    Smooth scroll

--- assets/js/main.js
+++ assets/js/main.js
@@ -460,17 +460,18 @@ function onLoad() {
 
     var ua = navigator.userAgent.toLowerCase();
     if (ua.indexOf('safari') != -1) {
-        updateScroller();
-        window.focus();
-        window.addEventListener("resize", onResize);
-        document.addEventListener("scroll", onScroll);
-        console.log('ua', ua.indexOf('safari') != -1)
-
-        /*if (ua.indexOf('chrome') > -1) {
+        if (ua.indexOf('chrome') > -1) {
             console.log('chrome');
+            updateScroller();
+            window.focus();
+            window.addEventListener("resize", onResize);
+            document.addEventListener("scroll", onScroll);
 
-
-
+        } else if (ua.indexOf('firefox') > -1) {
+            updateScroller();
+            window.focus();
+            window.addEventListener("resize", onResize);
+            document.addEventListener("scroll", onScroll);
         } else {
             console.log('safari');
             document.querySelector('.viewport').style.overflow = 'visible';
@@ -478,17 +479,8 @@ function onLoad() {
             document.querySelector('.scroll-container').style.overflow = 'visible';
             document.querySelector('.scroll-container').style.position = 'relative';
             document.querySelector('.scroll-container').style.transform = 'none';
-        }*/
-    }else {
-        console.log('safari');
-        document.querySelector('.viewport').style.overflow = 'visible';
-        document.querySelector('.viewport').style.position = 'relative';
-        document.querySelector('.scroll-container').style.overflow = 'visible';
-        document.querySelector('.scroll-container').style.position = 'relative';
-        document.querySelector('.scroll-container').style.transform = 'none';
+        }
     }
-
-
 }
 
 function updateScroller() {
