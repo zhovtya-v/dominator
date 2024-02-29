d89c2a3e588f62124d1cea17b502299446461d57
victoria.v <victoria.zhovtya.devmrks@gmail.com>
2024-02-29 14:53:28 +0200

    Smooth scroll

--- assets/js/main.js
+++ assets/js/main.js
@@ -164,11 +164,11 @@ let swiper = null;
 function about_reinit_slider() {
     if (mql.matches) {
         swiper = new Swiper('.about-swiper', {
-            grabCursor: true,
+            //grabCursor: true,
             spaceBetween: 12,
             slidesPerView: 1,
-            centeredSlides: true,
-            roundLengths: true,
+            //centeredSlides: true,
+            //roundLengths: true,
             loop: true,
 
             pagination: {
@@ -402,13 +402,12 @@ function buttonHoverAnimation() {
 
 buttonHoverAnimation();
 
-
 var html = document.documentElement;
 var body = document.body;
 
 var scroller = {
-    target: body,
-    ease: 0.5, // <= scroll speed
+    target: document.querySelector("#scroll-container"),
+    ease: 0.05, // <= scroll speed
     endY: 0,
     y: 0,
     resizeRequest: 1,
@@ -421,3 +420,53 @@ TweenLite.set(scroller.target, {
     rotation: 0.01,
     force3D: true
 });
+
+window.addEventListener("load", onLoad);
+
+function onLoad() {
+    updateScroller();
+    window.focus();
+    window.addEventListener("resize", onResize);
+    document.addEventListener("scroll", onScroll);
+}
+
+function updateScroller() {
+
+    var resized = scroller.resizeRequest > 0;
+
+    if (resized) {
+        var height = scroller.target.clientHeight;
+        body.style.height = height + "px";
+        scroller.resizeRequest = 0;
+    }
+
+    var scrollY = window.pageYOffset || html.scrollTop || body.scrollTop || 0;
+
+    scroller.endY = scrollY;
+    scroller.y += (scrollY - scroller.y) * scroller.ease;
+
+    if (Math.abs(scrollY - scroller.y) < 0.05 || resized) {
+        scroller.y = scrollY;
+        scroller.scrollRequest = 0;
+    }
+
+    TweenLite.set(scroller.target, {
+        y: -scroller.y
+    });
+
+    requestId = scroller.scrollRequest > 0 ? requestAnimationFrame(updateScroller) : null;
+}
+
+function onScroll() {
+    scroller.scrollRequest++;
+    if (!requestId) {
+        requestId = requestAnimationFrame(updateScroller);
+    }
+}
+
+function onResize() {
+    scroller.resizeRequest++;
+    if (!requestId) {
+        requestId = requestAnimationFrame(updateScroller);
+    }
+}
